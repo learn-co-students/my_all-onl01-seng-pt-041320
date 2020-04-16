7e57c60128412411641737488968922c73ccc62e
Dan Ste. Marie Jr <dste.mariejr@gmail.com>
2020-04-16 04:14:22 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
-def my_all?(collection)
-
+def my_all?(array)
+  collection = []
+  array.each {|beanie| collection << yield(collection)}
+  if collection.include?(false)
+    false
+  else
+    true
+  end
 end
